27321ede6c9aebfd4d38e9c8552402d2800d852f
dubemelchi <dubemelchi@gmail.com>
2019-10-09 19:11:58 -0400

    moved search logic to searchPagejs

---
+++ src/pages/searchPage.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import BookSearchForm from './components/bookSearchForm';
+import Loader from './components/loader';
+import BookList from './components/booksList';
+import './App.css';
+
+const SearchPage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [books, setBooks] = useState({ items: [] });
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const onInputChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  let API_URL = `https://www.googleapis.com/books/v1/volumes`;
+
+  const fetchBooks = async () => {
+    setLoading(true);
+    setError(false);
+    try {
+      const result = await axios.get(`${API_URL}?q=${searchTerm}`);
+      setBooks(result.data);
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    fetchBooks();
+  };
+
+  return (
+    <section>
+      <BookSearchForm
+        onSubmitHandler={onSubmitHandler}
+        onInputChange={onInputChange}
+        searchTerm={searchTerm}
+        error={error}
+      />
+      <Loader searchTerm={searchTerm} loading={loading} />
+      <BookList books={books} />
+    </section>
+  );
+};
+
+export default SearchPage;
